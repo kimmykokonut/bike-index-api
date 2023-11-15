3e9e652d430e1c86b2a3af39d6c7f0de7fccb8c8
Kim Robinson <kimmykokonut@gmail.com>
2023-11-15 11:42:08 -0800

    update error response for custom message
    Co-authored-by: Brianca Knight <briancaknight@gmail.com>

--- src/bike-service.js
+++ src/bike-service.js
@@ -3,8 +3,11 @@ export default class BikeService {
     return fetch(`https://bikeindex.org/api/v3/search?page=1&per_page=25&location=${zip}&distance=10&stolenness=proximity`)
       .then(function (response) {
         if (!response.ok) {
-          const errorMessage = `${response.status} ${response.statusText}${apiErrorMessage.messag}`;
-          throw new Error(errorMessage);
+          return response.json()
+            .then(function(apiErrorMessage) {
+              const errorMessage = `${response.status} ${response.statusText}${apiErrorMessage.message}`;
+              throw new Error(errorMessage);
+            });
         } else {
           return response.json();
         }
